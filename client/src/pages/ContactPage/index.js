ee245cb427c7b2ff476deb196fe1056af4e6f26d
Juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-06-04 15:54:01 -0700

    updated form

--- client/src/pages/ContactPage/index.js
+++ client/src/pages/ContactPage/index.js
@@ -11,14 +11,14 @@ const Contact = () => {
           <h3>Thanks for stopping by! You can contact me using this form or by sending me an email.</h3>
         </div>
 
-        <form name="contact v1" method="post" data-netlify="true" onSubmit="submit" netlify>
+        <form name="contact v1" method="post">
 
           <input type="hidden" name="form-name" value="contact v1" />
 
           <label className="contact-label" >Name:
             <input type="text" name="name" size="50" placeholder="Your name..." />
           </label>
-
+          
           <label className="contact-label" >Email:
             <input type="email" name="email" size="50" placeholder="Your email..." />
           </label>
