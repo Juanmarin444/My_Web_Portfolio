e5bc0fdfb1fd03ad1dcc856f718a291f3506dc72
Juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-06-19 16:52:16 -0700

    Added MERN section

--- client/src/pages/ProjectsPage/index.js
+++ client/src/pages/ProjectsPage/index.js
@@ -4,7 +4,8 @@ import {
   taskManagerData,
   courseTrackerData,
   worldClockData,
-  weatherAppData
+  weatherAppData,
+  smartShopData
 } from './projectsData';
 
 const Projects = () => {
@@ -12,14 +13,18 @@ const Projects = () => {
   return (
     <div className="projects">
       <h1>React Projects (PWA)</h1>
-      <div className="projects__react">
+      <div className="projects__type">
         <Project image={worldClockData.image} name={worldClockData.name} description={worldClockData.desc} url={worldClockData.url} />
         <Project image={weatherAppData.image} name={weatherAppData.name} description={weatherAppData.desc} url={weatherAppData.url} />
       </div>
       <h1>Django Projects</h1>
-      <div className="projects__django">
+      <div className="projects__type">
         <Project image={taskManagerData.image} name={taskManagerData.name} description={taskManagerData.desc} url={taskManagerData.url}/>
         <Project image={courseTrackerData.image} name={courseTrackerData.name} description={courseTrackerData.desc} url={courseTrackerData.url}/>
+      </div>
+      <h1>MERN Projects</h1>
+      <div className="projects__type">
+        <Project image={smartShopData.image} name={smartShopData.name} description={smartShopData.desc} url={smartShopData.url}/>
       </div>
     </div>
   )
