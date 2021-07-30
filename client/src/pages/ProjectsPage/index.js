0e255fde1f3e5058c54cf34f473b88e1182f890e
Juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-07-30 14:40:58 -0700

    Changed the order of projects

--- client/src/pages/ProjectsPage/index.js
+++ client/src/pages/ProjectsPage/index.js
@@ -12,19 +12,19 @@ const Projects = () => {
 
   return (
     <div className="projects">
-      <h1>React Projects (PWA)</h1>
+      <h1>MERN Projects</h1>
       <div className="projects__type">
-        <Project image={worldClockData.image} name={worldClockData.name} description={worldClockData.desc} url={worldClockData.url} />
-        <Project image={weatherAppData.image} name={weatherAppData.name} description={weatherAppData.desc} url={weatherAppData.url} />
+        <Project image={smartShopData.image} name={smartShopData.name} description={smartShopData.desc} url={smartShopData.url}/>
       </div>
       <h1>Django Projects</h1>
       <div className="projects__type">
         <Project image={taskManagerData.image} name={taskManagerData.name} description={taskManagerData.desc} url={taskManagerData.url}/>
         <Project image={courseTrackerData.image} name={courseTrackerData.name} description={courseTrackerData.desc} url={courseTrackerData.url}/>
       </div>
-      <h1>MERN Projects</h1>
+      <h1>React Projects (PWA)</h1>
       <div className="projects__type">
-        <Project image={smartShopData.image} name={smartShopData.name} description={smartShopData.desc} url={smartShopData.url}/>
+        <Project image={worldClockData.image} name={worldClockData.name} description={worldClockData.desc} url={worldClockData.url} />
+        <Project image={weatherAppData.image} name={weatherAppData.name} description={weatherAppData.desc} url={weatherAppData.url} />
       </div>
     </div>
   )
